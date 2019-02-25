723a2471f377f1f176d16df30982ba57d1121d6f
Michael Libois <libois.contact@gmail.com>
2019-02-25 15:22:13 +0100

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+//**HEADER - SMALL - BURGER-MENU-Action croix */
+$('.burger-menu').click(function(){
+    // console.log('hey');
+    $(this).add('.burger-menu').toggleClass('actioncroix');
+    $(this).add('.clique').toggleClass('actif');
+    $(this).add('body').toggleClass("hidden");
+})
+
+//**HEADER - SMALL -STICKY bar*//
+
+let hauteurHeader=$('header').outerHeight();
+$(window).scroll(function(){
+    let scrollActuel=$(window).scrollTop();
+    if(scrollActuel>hauteurHeader){
+        $('.sh_2').addClass('sticky');
+    }else{
+        $('.sh_2').removeClass('sticky');
+    }    
+});
